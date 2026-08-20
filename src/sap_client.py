78cdc6851ff86bd220de3f820fb46f84c725d787
Laura Zhang <laura.zhang167@gmail.com>
2026-08-20 15:35:11 -0400

    Refactor SAPClient to encapsulate API interactions and improve data retrieval methods

--- src/sap_client.py
+++ src/sap_client.py
@@ -1,16 +1,40 @@
 import requests, os
+import pandas as pd
 
-api_key = os.getenv("SAP_API_KEY")
-url = 'https://sandbox.api.sap.com/successfactorsfoundation/odata/v2/User?%24top=20'
+class SAPClient:
 
-headers = {
-    'APIKey': api_key,
-    'Accept': 'application/json',
-    'DataServiceVersion': '2.0',
-}
+    def __init__(self):
+        self.base_url = os.getenv("SAP_BASE_URL")
+        self.api_key = os.getenv("SAP_API_KEY")
 
-response = requests.get(url, headers=headers)
-response.raise_for_status()  # Raise an error for bad responses
-data = response.json()
+    def _get(self, endpoint, params=None):
+        response = requests.get(
+            f"{self.base_rul}/{endpoint}",
+            headers={
+                "APIKey": self.api_key,
+                "Accept": "application/json",
+                "DataServiceVersion": '2.0'
+            },
+            params=params
+        )
+    def get_ibase_records(self):
+        data = self._get(
+            'sap/c4c/odata/v1/c4codataapi/InstalledBaseCollection',
+            params={
+                "$select": "ObjectID,AddressLine1"
+            }
+        )
+        return pd.DataFrame(data["results"])
 
-print(data)
+    def get_work_reports(self):
+        data = self._get(
+            'sapassetintelligencenetwork/workorders',
+            params={
+                "$select": "equipmentId, location"
+            }
+        )
+        return pd.DataFrame(data)
+
+    def update_ibase_location(self):
+        # also update LastChangedOn
+        pass
